825dbb1d3d65170bf82c5f2654e8291b3ddcd662
Dustin Sallings <dustin@spy.net>
2008-10-20 02:11:34 -0700

    Turned the example test into a test suite.

--- example/test.py
+++ example/test.py
@@ -5,30 +5,79 @@ import sys
 sys.path.append("..")
 sys.path.append(os.path.join(sys.path[0], '..'))
 
-from twisted.internet import reactor, protocol
+from twisted.internet import reactor, protocol, defer
 
 import beanstalk
+
+testDeferreds=[]
+failures=[]
 
 d=protocol.ClientCreator(reactor, beanstalk.Beanstalk).connectTCP(
     sys.argv[1], 11300)
 
+def print_cb(name):
+    def f(val):
+        print name, `val`
+    return f
+
+def success_print(v):
+    sys.stdout.write(".")
+    sys.stdout.flush()
+
+def handle_failure(cmd):
+    def f(v):
+        sys.stdout.write("E")
+        sys.stdout.flush()
+        failures.append((cmd, v))
+    return f
+
+def handle_unexpected_success(cmd):
+    def f(v):
+        # XXX:  Why would I get callback on error?
+        if v is None:
+            sys.stdout.write(".")
+        else:
+            sys.stdout.write("E")
+            failures.append((cmd, v))
+        sys.stdout.flush()
+    return f
+
+def success(f, *args):
+    rv = f(*args).addCallback(success_print).addErrback(
+        handle_failure(f.__name__))
+    testDeferreds.append(rv)
+    return rv
+
+def failure(f, *args):
+    rv = f(*args).addErrback(success_print).addCallback(
+        handle_unexpected_success(f.__name__))
+    testDeferreds.append(rv)
+    return rv
+
 def runCommands(bs):
-    def p(name):
-        def f(val):
-            print name, `val`
-        return f
-    bs.stats().addCallback(p("stats"))
-    bs.use("crack").addCallback(p("crack"))
-    bs.watch("tv").addCallback(p("watch"))
-    bs.ignore("tv").addCallback(p("ignore"))
-    bs.watch('crack')
-    bs.put(8192, 0, 300, 'This is a job').addCallback(p("put"))
+    success(bs.stats)
+    success(bs.use, "crack")
+    success(bs.watch, "tv")
+    success(bs.ignore, "tv")
+    success(bs.watch, "crack")
+    success(bs.put, 8192, 0, 300, 'This is a job')
     def runJob(j):
         id, job=j
-        print "Running job:  ", j
-        bs.delete(id).addCallback(p("deleted"))
+        success_print(None)
+        return success(bs.delete, id)
     bs.reserve(1).addCallback(runJob)
-    bs.reserve(1).addErrback(p("failing reserve"))
+    failure(bs.reserve, 1)
+
+    dl=defer.DeferredList(testDeferreds)
+    def done(v):
+        sys.stdout.write("\n")
+        print "Finished all tests."
+        if failures:
+            print "Failures:"
+            for f in failures:
+                print "\t" + `f`
+        reactor.stop()
+    dl.addCallback(done)
 
 d.addCallback(runCommands)
 
