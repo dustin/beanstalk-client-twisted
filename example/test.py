360e06ed552b3c3296265a4bdadd6a122bfdc0d5
Dustin Sallings <dustin@spy.net>
2008-10-20 00:29:58 -0700

    My example script.

---
+++ example/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import sys
+sys.path.append("..")
+sys.path.append(os.path.join(sys.path[0], '..'))
+
+print `sys.path`
+from twisted.internet import reactor, protocol
+
+import beanstalk
+
+d=protocol.ClientCreator(reactor, beanstalk.Beanstalk).connectTCP(
+    sys.argv[1], 11300)
+
+def runCommands(bs):
+    def p(name):
+        def f(val):
+            print name, `val`
+        return f
+    bs.stats().addCallback(p("stats"))
+    bs.use("crack").addCallback(p("crack"))
+    bs.watch("tv").addCallback(p("watch"))
+    bs.ignore("tv").addCallback(p("ignore"))
+    bs.watch('crack')
+    bs.put(8192, 0, 300, 'This is a job').addCallback(p("put"))
+    def runJob(j):
+        id, job=j
+        print "Running job:  ", j
+        bs.delete(id).addCallback(p("deleted"))
+    bs.reserve(1).addCallback(runJob)
+    bs.reserve(1).addErrback(p("failing reserve"))
+
+d.addCallback(runCommands)
+
+reactor.run()
